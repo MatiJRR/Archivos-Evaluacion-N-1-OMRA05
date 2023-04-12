355fa49dbaa1733be51bd4f537d04196321f7920
NetAcad DEVASC <changeme@example.com>
2023-04-12 14:10:56 +0000

    Mensaje Evaluacion N°1

---
+++ informacion.py
@@ -0,0 +1,5 @@
+nombres=input("Cuales son sus nombres?: ")
+apellidos=input("Cuales son sus apellidos?: ")
+seccion=(input("Escriba el código sección: "))
+sede=input("A que sede pertenece?: ")
+print("Hola! "+nombres+" "+apellidos+" "+seccion+" "+sede+".")
---
+++ script.py
@@ -0,0 +1,2 @@
+print("Evaluación N°1 Redes Avanzadas I")
+print("Rodrigo Flores y Matías Inzulza")
